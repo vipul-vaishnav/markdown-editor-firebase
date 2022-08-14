a4a8ea30444ea5412a3cb14eeca6a39526ed225d
Vipul Vaishnav <007vipulrank1@gmail.com>
2022-08-14 22:52:16 +0530

    show or hide preview add

--- src/components/EditorMain.jsx
+++ src/components/EditorMain.jsx
@@ -2,8 +2,10 @@ import React, { useState, useRef } from 'react';
 import EyeIcon from './../icons/EyeIcon';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import EyeOffIcon from './../icons/EyeOffIcon';
 
 const EditorMain = ({ lightMode }) => {
+  const [hideMD, setHideMD] = useState(false);
   const [value, setValue] = useState('# Welcome to *MARKDOWN*');
   const ref = useRef();
   const handleHeight = (e) => {
@@ -15,25 +17,27 @@ const EditorMain = ({ lightMode }) => {
   return (
     <>
       <div className="flex items-center justify-between w-full bg-tertiary">
+        {!hideMD && (
+          <div
+            className={`flex items-center  justify-between w-full h-10 px-4 font-bold tracking-widest text-left text-gray-300 uppercase md:border-r border-${
+              lightMode ? 'gray-500' : 'white'
+            }`}
+          >
+            Markdown
+            <button className="md:hidden" onClick={() => setHideMD((p) => !p)}>
+              {hideMD ? <EyeOffIcon /> : <EyeIcon />}
+            </button>
+          </div>
+        )}
         <div
-          className={`flex items-center  justify-between w-full h-10 px-4 font-bold tracking-widest text-left text-gray-300 uppercase md:border-r border-${
-            lightMode ? 'gray-500' : 'white'
-          }`}
-        >
-          Markdown
-          <button>
-            <EyeIcon />
-          </button>
-        </div>
-        <div
-          className={`items-center justify-between hidden w-full px-4 py-2 font-bold tracking-widest text-left text-gray-300 uppercase border-${
+          className={`flex items-center justify-between ${
+            !hideMD && 'hidden'
+          } w-full px-4 py-2 font-bold tracking-widest text-left text-gray-300 uppercase border-${
             lightMode ? 'gray-500' : 'white'
           } md:border-l md:flex`}
         >
           Preview
-          <button>
-            <EyeIcon />
-          </button>
+          <button onClick={() => setHideMD((p) => !p)}>{hideMD ? <EyeOffIcon /> : <EyeIcon />}</button>
         </div>
       </div>
 
@@ -45,7 +49,9 @@ const EditorMain = ({ lightMode }) => {
       >
         {/* MARKDOWN */}
         <div
-          className={`w-full h-full px-4 py-4 overflow-x-hidden overflow-y-auto text-left border-r-0 border-${
+          className={`w-full ${
+            hideMD && 'hidden'
+          } h-full px-4 py-4 overflow-x-hidden overflow-y-auto text-left border-r-0 border-${
             lightMode ? 'gray-500' : 'white'
           } md:border-r`}
         >
@@ -62,7 +68,7 @@ const EditorMain = ({ lightMode }) => {
         </div>
         {/* PREVIEW */}
         <div
-          className={`hidden prose prose-a:text-blue-500 prose-headings:text-gray-900 max-w-full ${
+          className={`${!hideMD && 'hidden'} prose prose-a:text-blue-500 prose-headings:text-gray-900 max-w-full ${
             !lightMode && 'prose-invert prose-headings:text-white'
           } text-current w-full h-full px-4 py-4 overflow-x-hidden overflow-y-auto text-left border-l-0 border-${
             lightMode ? 'gray-500' : 'white'
